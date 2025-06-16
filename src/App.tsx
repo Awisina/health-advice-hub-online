b5f44822285c6aee96438e34c5b49416bea62f68
Farruh Sheripov <farruh.sheripov@fusioncode.org>
2025-06-16 20:49:06 +0300

    fix: Update BrowserRouter basename for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="health-advice-hub-online">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/services" element={<ServicesPage />} />
